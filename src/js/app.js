ee2a493c1311ad0706f35b0af6aabffa8f53f7d1
Alireza Pourgholam <Alireza.pourgholam444@gmail.com>
2024-05-07 21:57:21 +0330

    updated

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ todoForm.addEventListener("submit" , addNewTodo);
 
      function addNewTodo(e) {
      e.preventDefault();
-     if(!todoTitleInput.value , !todoDescriptionInput.value) return null;
+     if(!todoTitleInput.value || !todoDescriptionInput.value) return null;
      const todo = {
           id: Date.now(),
           title: todoTitleInput.value,
@@ -33,6 +33,9 @@ showUnCompletedTodos(newTodosUnCompleted);
      const removebtn = [...document.querySelectorAll(".delete-btn")];
      removebtn.forEach((btn) => btn.addEventListener("click" , removeTodo));
 
+     const editbtn = [...document.querySelectorAll(".edit-btn")];
+     editbtn.forEach((btn) => btn.addEventListener("click" , editTodo));
+
      const checkbtn = [...document.querySelectorAll("#todo-radio-item")];
      checkbtn.forEach((btn) => btn.addEventListener("click" , checkTodo));
      }
@@ -42,9 +45,27 @@ showUnCompletedTodos(newTodosUnCompleted);
       const filteredTodo = todos.filter((t) => t.id !== todoId);
       createTodos(filteredTodo);
     }
+
+    function editTodo() {
+      const editSection = document.querySelector(".edit-section");
+      const submitEditBtn = document.querySelector(".submit-edit-btn");
+      editSection.style.display = "block";
+      submitEditBtn.addEventListener("click" , submitEditTodo);
+
+    }
+    function submitEditTodo(e) {
+      const newTitleInputEdit = document.querySelector(".newTitleInput");
+      const todoId = Number(e.target.dataset.todoId);
+      const todo = todos.find((t) => t.id == todoId);
+      todo.title = newTitleInputEdit.value;
+      createTodos(todos);
+    }
+
+
     function checkTodo(e) {
       const todoId = Number(e.target.dataset.todoId);
       const todo = todos.find((t) => t.id == todoId);
+
       todo.isComplete = !todo.isComplete;
       createTodos(todos);
     }
@@ -78,17 +99,28 @@ const showUnCompletedTodos = (todos) => {
     id="todo-item">
     <div class="flex items-center">
       <input data-todo-id=${todo.id} id="todo-radio-item" aria-describedby="helper-checkbox-text"
-        type="checkbox" value=""
+        type="checkbox"
         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-200 focus:ring-2 dark:bg-white dark:border-gray-300">
     </div>
     <div class="ms-4 flex flex-1 flex-col">
+
+    <div style="display: none;" class="edit-section relative m-2">
+    <input type="text" value=${todo.title} data-todo-id=${todo.id}
+      class="newTitleInput small-input w-full block p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Type Your Title..." />
+    <button type="submit" data-todo-id=${todo.id}
+      class="submit-edit-btn text-white items-start justify-start absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Submit</button>
+  </div>
+
       <label class="todo-title text-base font-bold text-slate-600" id="todo-text">${todo.title}</label>
       <span class="todo-description text-sm text-slate-500"
         id="todo-description-user">${todo.description}</span>
     </div>
     <div class="flex flex-row gap-x-4">
+    <button data-todo-id=${todo.id} class="edit-btn text-slate-600 hover:text-slate-800"> Edit </button>
       <button data-todo-id=${todo.id} class="delete-btn text-red-600 hover:text-red-800"> Delete </button>
+
     </div>
-  </li>`
+  </li> `
   ))
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html" , "./src/**/*.js"],
+  content: ["./public/**/*.html" , "./src/**/*.js" ,"./node_modules/flowbite/**/*.js"],
+  
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
   ],
 }
 
